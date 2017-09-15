3742b77be5350a3d949ee04e19dae6d1b1132d4c
Artem Lapkin <artemka666@mail.ru>
2017-09-15 19:01:13 +0300

    Всё

--- js/script.js
+++ js/script.js
@@ -1,23 +1,2 @@
-var popUpShow = document.querySelector(".contacts-button");
-var popUp = document.querySelector(".feedback-section");
-var popUpClose = document.querySelector(".feedback-close");
-var popUpLogin = popUp.querySelector("[id=your-name]");
-var popUpMail = popUp.querySelector("[id=your-mail]");
-var popUpText = popUp.querySelector("[id=text]");
-var form = popUp.querySelector("form");
-
-popUpShow.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popUp.classList.remove("hidden");
-    popUpLogin.focus();
-});
-
-popUpClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popUp.classList.add("hidden");
-});
-
-form.addEventListener("submit", function (evt) {
-    evt.preventDefault();
-    console.log(popUpLogin.value);
-});
+var popUpShow=document.querySelector(".contacts-button"),popUp=document.querySelector(".feedback-section"),popUpClose=document.querySelector(".feedback-close"),popUpLogin=popUp.querySelector("[id=your-name]"),popUpMail=popUp.querySelector("[id=your-mail]"),popUpText=popUp.querySelector("[id=text]"),form=popUp.querySelector("form");popUpShow.addEventListener("click",function(a){a.preventDefault();popUp.classList.remove("hidden");popUpLogin.focus()});
+popUpClose.addEventListener("click",function(a){a.preventDefault();popUp.classList.add("hidden")});form.addEventListener("submit",function(a){a.preventDefault();console.log(popUpLogin.value)});
